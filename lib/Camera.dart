67054a9057cfb801e9a1aa616abeec5ea1e26f08
ntua-el19952 <el19952@mail.ntua.gr>
2023-02-20 22:07:24 +0200

    Camera start

---
+++ lib/Camera.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class StoryCamera extends StatefulWidget {
+  const StoryCamera({super.key});
+
+  @override
+  State<StoryCamera> createState() => _StoryCameraState();
+}
+
+class _StoryCameraState extends State<StoryCamera> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
