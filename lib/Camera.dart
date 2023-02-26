c1dbf122fbc8fed74fa471dc50df45458912d5b3
ntua-el19952 <el19952@mail.ntua.gr>
2023-02-26 21:17:29 +0200

    minor change

--- lib/Camera.dart
+++ lib/Camera.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:image_picker/image_picker.dart';
 
 class StoryCamera extends StatefulWidget {
   final List<CameraDescription>? cameras;
